3203e8a459c382822e0303a0e744878f8de87ba5
abderrahmanajid <abderrahman.ajid@gmail.com>
2024-08-27 11:05:30 +0100

    Initial Commit

---
+++ SimpleMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMovies
+//
+//  Created by Abderrahman Ajid on 27/8/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
